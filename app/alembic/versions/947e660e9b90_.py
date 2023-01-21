ffe8304ed789c94ac1b78052396b9eeac0f9f4bd
Szymon Sawczuk <szymon@Szymons-MacBook-Air.local>
2023-01-21 12:37:35 +0100

    Repair migrations head

---
+++ app/alembic/versions/947e660e9b90_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 947e660e9b90
+Revises: fd8207b9224f, efd41ed3f3ef
+Create Date: 2023-01-21 12:35:39.541516
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '947e660e9b90'
+down_revision = ('fd8207b9224f', 'efd41ed3f3ef')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
